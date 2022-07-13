0ea14117ec1b4826a8951779e84af32e141e03b7
ThiagoZardo <thiago.artevisual@gmail.com>
2022-07-13 17:15:12 -0300

    Req 02 ok

---
+++ src/database/models/user.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, Datatypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: Datatypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: Datatypes.STRING,
+    email: Datatypes.STRING,
+    password: Datatypes.STRING,
+    image: Datatypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'Users',
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts, { foreignKey: 'userId', as: 'blogPosts' });
+  };
+
+  return User
+};
